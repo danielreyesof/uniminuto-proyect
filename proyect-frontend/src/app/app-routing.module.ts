babc8ad3482e4dd09236d9c2db8fa34ccb538080
Miguel Merchan <majimenez525@misena.edu.co>
2022-08-11 23:48:11 -0500

    User panel

--- proyect-frontend/src/app/app-routing.module.ts
+++ proyect-frontend/src/app/app-routing.module.ts
@@ -5,13 +5,39 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuardIn } from './guards/auth.guard';
 import { SignInComponent } from './pages/auth/sign-in/sign-in.component';
 import { SignUpComponent } from './pages/auth/sign-up/sign-up.component';
+import { UserComponent } from './pages/user/user.component';
+import { DetachablesComponent } from './pages/user/detachables/detachables.component';
+import { PersonalDataComponent } from './pages/user/personal-data/personal-data.component';
+import { PreferencesComponent } from './pages/user/preferences/preferences.component';
+import { MainAppComponent } from './pages/main-app/main-app.component';
 
 const routes: Routes = [
   {
     path: '',
     component: AppComponent,
-    canActivate: [AuthGuardIn],
-    children: [{ path: '', component: LandingComponent }],
+    children: [
+      { path: '', component: LandingComponent },
+
+      {
+        path: 'app',
+        component: MainAppComponent,
+        children: [
+          {
+            path: 'user', 
+            component: UserComponent, 
+            children: [
+              {
+                path: 'detachables', component: DetachablesComponent
+              },
+              {
+                path: 'personal', component: PersonalDataComponent
+              },
+              {
+                path: 'preferences', component: PreferencesComponent
+              }
+            ]
+          }]
+      }],
   },
   {
     path: 'sign-in',
@@ -27,4 +53,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
---
+++ proyect-frontend/src/app/pages/user/detachables/detachables.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detachables',
+  templateUrl: './detachables.component.html',
+  styleUrls: ['./detachables.component.scss']
+})
+export class DetachablesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
